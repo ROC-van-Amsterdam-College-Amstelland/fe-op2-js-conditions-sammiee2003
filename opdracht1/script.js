b2dea8408bc31cb06a6533468822aa8ba6e76190
sammiee2003 <samvandenbosch2003@gmail.com>
2019-11-13 11:19:44 +0100

    opdracht 1

--- opdracht1/script.js
+++ opdracht1/script.js
@@ -1,25 +1,27 @@
 // A) maak 2 verschillende variabelen met twee willekeurige integers (zelf kiezen)
 
 // jouw code hierondeer-->> 
-var ;
-var ;
+var getal1 = 77; 
+var getal2 = 66;
 
 // B) toon beide getallen op het scherm met document.getElementById('<jouw_gekozen_id>').innerHTML
 
 // jouw code hieronder-->>
-document.getElementById
+document.getElementById("getal1").innerHTML = getal1;
+document.getElementById("getal2").innerHTML = getal2;
 
 // C) Vraag de gebruiker om een INPUT waarde door prompt() te gebruiken. 
 // Gebruik de prompt()-waarde om een variabelen te 'vullen'
 
 // jouw code hieronder-->>
-var ;
-var ;
+var getal1 = prompt("typ getal");
+var getal2 = prompt("typ getal"); 
+
 
 // D) Vergelijk de twee getallen met elkaar door gebruik te maken van conditions  if()
 
 // jouw code hieronder-->>
-if () {
-
+if (getal1 > getal2) {
+    alert("getal 1 is groter dan getal 2")
 }
 
