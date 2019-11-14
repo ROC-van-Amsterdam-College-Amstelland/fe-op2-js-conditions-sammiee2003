6d1ff01a7d537af59f8c9f3a10221dda2e42fda6
sammiee2003 <samvandenbosch2003@gmail.com>
2019-11-14 15:22:21 +0100

    java geschreven

--- hw-opdracht2/script.js
+++ hw-opdracht2/script.js
@@ -5,18 +5,35 @@ function zoekUit() {
 
     var abonnement ="eenvoudig";
 //als je met meer dan 2 personen kijkt en meer dan 3 uur per dag, dan is het beste abonnement familie
-//pseudocode: als (aantal==3 en duur>3 dan abonnement ="familie")
+//pseudocode: als (aantal ==3 en duur>3 dan abonnement ="familie")
+
+if( aantal == 3 && duur > 3){
+alert();
+ abonnement = "familie";
+}
+
 
 //als je met 2 of meer personen kijkt of al een abonnement hebt, dan is het beste abonnement uitgebreid
-//pseudocode: 
+//pseudocode: als 
+if ( aantal > 2 || streaming == true){
+    abonnement = "uitgebreid";
+
+}
 
 //als je met 2 personen kijkt of meer dan 4 uur per dag dan is het abonnement standaard
 //pseudocode: 
+if(aantal == 2 || duur > 4){
+    abonnement = "standaard";
+}
 
 // als je met 1 persoon kijkt en 6 of meer uur per dag dan is het abonnement uitgebreid
 //pseudocode: 
+if(aantal == 1 && duur >= 6){
+    abonnement = "uitgebreid";
+    
+}
 
-
+document.getElementById("resultaat").innerHTML = abonnement;
 //toon het type abonnement op het scherm
 
 }
