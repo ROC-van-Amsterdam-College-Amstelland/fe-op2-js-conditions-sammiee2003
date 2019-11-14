beffd9a9d87319a099f9d95674b6c354b26c3453
sammiee2003 <samvandenbosch2003@gmail.com>
2019-11-14 14:35:21 +0100

    c in hoofdletter veranderd

--- opdracht2/script.js
+++ opdracht2/script.js
@@ -5,15 +5,26 @@
  */
 
 /* jouw code hieronder-->> */
-var ingevuld_getal = ;
+var ingevuld_getal = document.getElementById("leeftijd").value; 
+
 
 /* B) Schrijf code waarmee je het ingevulde getal vergelijkt (condition) met de stemgerechtigde leeftijd van 18 jaar.
  * Als de leeftijd groter is dan 18 dan verander je de achtergrondkleur van de site met:
                     document.getElementByTagName('body').style.backgroundColor = 'purple'
  */
-
+var getal1 = 18;
 /* jouw code hieronder-->> */
+function check() {
+    var ingevuld_getal = document.getElementById("leeftijd").value; 
 
+    if(ingevuld_getal > getal1){
+        document.getElementById("mainBody").style.backgroundColor = 'purple';
+     }
+
+     if(ingevuld_getal < getal1){
+        document.getElementById("mainBody").style.backgroundColor = 'green';
+     }
+}
 
 
 /* C) Schrijf code waarmee je het ingevulde getal vergelijkt (condition) met de stemgerechtigde leeftijd van 18 jaar.
